44590b9e8952258e71abaa69486f8c8271ada431
GMendozaAraque <108754722+GMendozaAraque@users.noreply.github.com>
2024-09-19 18:37:59 -0600

    index actualizado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,62 @@
+// import React, { useEffect } from 'react';
+// import AOS from 'aos';
+// import "aos/dist/aos.css";
+// import './index.css';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from 'react-router-dom';
+// // All pages
+// import Home from './pages/Home';
+// import Contact from './pages/Contact';
+// import DemoProduct from './pages/DemoProduct';
+
+// import {useDocTitle} from './components/CustomHook';
+// import ScrollToTop from './components/ScrollToTop';
+
+// function App() {
+//   useEffect(() => {
+//     const aos_init = () => {
+//       AOS.init({
+//         once: true,
+//         duration: 1000,
+//         easing: 'ease-out-cubic',
+//       });
+//     }
+
+//     window.addEventListener('load', () => {
+//       aos_init();
+//     });
+//   }, []);
+
+//   useDocTitle("CommunityTech");
+
+//   return (
+//     <>
+//       <Router>
+//         <ScrollToTop>
+//           <Routes>
+//             <Route path="/" element={<Home />} />
+//             <Route path="/contact" element={<Contact />} />
+//             <Route path="/get-demo" element={<DemoProduct />} /> 
+//           </Routes>
+//         </ScrollToTop>
+//       </Router>
+//     </>
+//   );
+// }
+
+
+// export default App;
+
+
 import React, { useEffect } from 'react';
 import AOS from 'aos';
 import "aos/dist/aos.css";
 import './index.css';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route
 } from 'react-router-dom';
@@ -12,7 +65,7 @@ import Home from './pages/Home';
 import Contact from './pages/Contact';
 import DemoProduct from './pages/DemoProduct';
 
-import {useDocTitle} from './components/CustomHook';
+import { useDocTitle } from './components/CustomHook';
 import ScrollToTop from './components/ScrollToTop';
 
 function App() {
@@ -33,19 +86,16 @@ function App() {
   useDocTitle("CommunityTech");
 
   return (
-    <>
-      <Router>
-        <ScrollToTop>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/get-demo" element={<DemoProduct />} /> 
-          </Routes>
-        </ScrollToTop>
-      </Router>
-    </>
+    <Router>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/get-demo" element={<DemoProduct />} />
+        </Routes>
+      </ScrollToTop>
+    </Router>
   );
 }
-
 
 export default App;
